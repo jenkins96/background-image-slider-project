5892f9cea39c0251a7ceb2c37ff1fc6126dc37e8
jenkins96 <adrianjnkns@gmail.com>
2021-02-22 23:15:10 +0000

    currently working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-// immediate invoked function expression
-
 (function() {
   const pictures = [
     "contBcg-0",
@@ -9,27 +7,6 @@
     "contBcg-4"
   ];
 
-  //select both left and right button. Add event listeners
-  const buttons = document.querySelectorAll('.btn')
-  const imageDIV = document.querySelector('.img-container')
-  let counter = 0
 
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      if (button.classList.contains('btn-left')){
-        counter--
-        if(counter < 0){
-          counter = pictures.length -1
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-      if (button.classList.contains('btn-right')){
-        counter++
-        if(counter > pictures.length -1){
-          counter = 0
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-    })
-  })
+  
 })();

5174d87096fd8b863812e9886ec32e552bdd1e13
jenkins96 <adrianjnkns@gmail.com>
2021-02-25 19:45:15 +0000

    files

--- js/app.js
+++ js/app.js
@@ -1,27 +1,39 @@
-(function() {
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
-// 
-let index = 0
-const buttons = document.querySelectorAll("button");
-// For Each
-buttons.forEach((button) => {
-// Event Listener
-button.addEventListener("click", function(){
-    if(button.classList.contains("btn-left")){
-        index++;
-    } else if(button)
-})
-
-
-
-})
-
-
-  
+// IIFE
+(function () {
+    // Array of pictures (pictures in "img" folder)
+    const pictures = [
+        "contBcg-0",
+        "contBcg-1",
+        "contBcg-2",
+        "contBcg-3",
+        "contBcg-4"
+    ];
+    // Selecting buttons
+    const buttons = document.querySelectorAll(".btn");
+    // Selecting image location on HTML
+    const imageLocation = document.querySelector(".img-container");
+    // Declaring index to use for selecting a picture
+    let index = 0;
+    // Buttons forEach
+    buttons.forEach(function(element){
+    // Event listener for each element
+    element.addEventListener('click', function(e){
+      if (element.classList.contains('btn-left')){ // Selecting left button
+        index--
+        if(index < 0){
+          index = pictures.length -1 // if it goes beyond "0" its value will be "4"
+        }
+        // Setting the background
+        imageLocation.style.backgroundImage = `url('./img/${pictures[index]}.jpeg')` 
+      }
+      if (element.classList.contains('btn-right')){ // Selecting right button
+        index++
+        if(index > pictures.length -1){
+          index = 0  // if it goes above "4" its value will be "0"
+        }
+        // Setting the background
+        imageLocation.style.backgroundImage = `url('./img/${pictures[index]}.jpeg')`
+      }
+    })
+  })
 })();
